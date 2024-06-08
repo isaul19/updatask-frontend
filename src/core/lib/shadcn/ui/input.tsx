3010f140e321dd73f1db0cb96574f9fc8b811966
isaul19 <saulhola348@gmail.com>
2024-06-08 14:27:46 -0500

    feat(shadcn): add error message in input and textarea

--- src/core/lib/shadcn/ui/input.tsx
+++ src/core/lib/shadcn/ui/input.tsx
@@ -2,19 +2,24 @@ import * as React from "react";
 
 import { cn } from "@lib/shadcn/utils";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  errorMessage?: string;
+}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, errorMessage, type, ...props }, ref) => {
   return (
-    <input
-      type={type}
-      className={cn(
-        "flex h-10 w-full rounded-md border border-slate-200 bg-white px-3 py-6 text-base ring-offset-white file:border-0 file:bg-transparent file:text-base file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300 mt-2.5",
-        className,
-      )}
-      ref={ref}
-      {...props}
-    />
+    <>
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-slate-200 bg-white px-3 py-6 text-base ring-offset-white file:border-0 file:bg-transparent file:text-base file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300 mt-2.5",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+      <div>{errorMessage && <p className="text-red-500 mt-2 text-sm">{errorMessage}</p>}</div>
+    </>
   );
 });
 Input.displayName = "Input";
--- src/core/lib/shadcn/ui/textarea.tsx
+++ src/core/lib/shadcn/ui/textarea.tsx
@@ -2,18 +2,23 @@ import * as React from "react";
 
 import { cn } from "@lib/shadcn/utils";
 
-export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  errorMessage?: string;
+}
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ className, ...props }, ref) => {
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ className, errorMessage, ...props }, ref) => {
   return (
-    <textarea
-      className={cn(
-        "flex min-h-[80px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-base ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary  focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300 mt-2.5",
-        className,
-      )}
-      ref={ref}
-      {...props}
-    />
+    <>
+      <textarea
+        className={cn(
+          "flex min-h-[80px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-base ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary  focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300 mt-2.5",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+      <div>{errorMessage && <p className="text-red-500 mt-2 text-sm">{errorMessage}</p>}</div>
+    </>
   );
 });
 Textarea.displayName = "Textarea";
