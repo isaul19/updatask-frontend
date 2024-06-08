4d5d77bb573ada0622282a21d6742568e619965f
isaul19 <saulhola348@gmail.com>
2024-06-08 15:23:04 -0500

    feat(shadcn): update colors in error message

--- src/core/lib/shadcn/ui/textarea.tsx
+++ src/core/lib/shadcn/ui/textarea.tsx
@@ -11,13 +11,13 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ classNa
     <>
       <textarea
         className={cn(
-          "flex min-h-[80px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-base ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary  focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300 mt-2.5",
+          "flex min-h-[80px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-base ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary  focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300 font-normal mt-2.5",
           className,
         )}
         ref={ref}
         {...props}
       />
-      <div>{errorMessage && <p className="text-red-500 mt-2 text-sm">{errorMessage}</p>}</div>
+      <div>{errorMessage && <p className="bg-red-400 py-2 text-white text-center mt-2 text-sm">{errorMessage}</p>}</div>
     </>
   );
 });
